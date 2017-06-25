1c84962cafd6e361b2d6a0f8b24b39f5477bf3d4
Christian <christian.rois.cr@gmail.com>
2017-06-25 23:19:21 +0200

    Lagerverwaltung - KlickEvent für Raster und Optische Anpassungen

--- lager/d_lager.php
+++ lager/d_lager.php
@@ -2,53 +2,70 @@
 <div class="lager_container">
     <div class="row lager_reihe lager_quer_linie">
         <div class="col-md-4 lager_fach">
-            <button type="button" class="lager_fach_button">
+            <button type="button" class="lager_fach_button" id="A1">
                 <span class="glyphicon glyphicon-plus lager_glyph" aria-hidden="true"></span>
             </button>
         </div>
         <div class="col-md-4 lager_fach_mitte">
-            <button type="button" class="lager_fach_button">
+            <button type="button" class="lager_fach_button" id="A2">
                 <span class="glyphicon glyphicon-plus lager_glyph" aria-hidden="true"></span>
             </button>
         </div>
         <div class="col-md-4 lager_fach">
-            <button type="button" class="lager_fach_button">
+            <button type="button" class="lager_fach_button" id="A3">
                 <span class="glyphicon glyphicon-plus lager_glyph" aria-hidden="true"></span>
             </button>
         </div>
     </div>
     <div class="row lager_reihe lager_quer_linie">
         <div class="col-md-4 lager_fach">
-            <button type="button" class="lager_fach_button">
+            <button type="button" class="lager_fach_button" id="B1">
                 <span class="glyphicon glyphicon-plus lager_glyph" aria-hidden="true"></span>
             </button>
         </div>
         <div class="col-md-4 lager_fach_mitte">
-            <button type="button" class="lager_fach_button">
+            <button type="button" class="lager_fach_button" id="B2">
                 <span class="glyphicon glyphicon-plus lager_glyph" aria-hidden="true"></span>
             </button>
         </div>
         <div class="col-md-4 lager_fach">
-            <button type="button" class="lager_fach_button">
+            <button type="button" class="lager_fach_button" id="B3">
                 <span class="glyphicon glyphicon-plus lager_glyph" aria-hidden="true"></span>
             </button>
         </div>
     </div>
     <div class="row lager_reihe">
         <div class="col-md-4 lager_fach">
-            <button type="button" class="lager_fach_button">
+            <button type="button" class="lager_fach_button" id="C1">
                 <span class="glyphicon glyphicon-plus lager_glyph" aria-hidden="true"></span>
             </button>
         </div>
         <div class="col-md-4 lager_fach_mitte">
-            <button type="button" class="lager_fach_button">
+            <button type="button" class="lager_fach_button" id="C2">
                 <span class="glyphicon glyphicon-plus lager_glyph" aria-hidden="true"></span>
             </button>
         </div>
         <div class="col-md-4 lager_fach">
-            <button type="button" class="lager_fach_button">
+            <button type="button" class="lager_fach_button" id="C3">
                 <span class="glyphicon glyphicon-plus lager_glyph" aria-hidden="true"></span>
             </button>
         </div>
     </div>
-</div>        
+</div>  
+<div>
+    <div class="row">
+        <div class="col-md-8">
+            <h2><button type="button" class="lager_change_button" id="regalChangeLeft">
+                <span class="glyphicon glyphicon-arrow-left" aria-hidden="true"></span>
+            </button>
+            <button type="button" class="lager_change_button" id="regalChangeRight">
+                <span class="glyphicon glyphicon-arrow-right" aria-hidden="true"></span>
+            </button></h2>
+        </div>
+        <div class="col-md-4">
+            <h2 id="reagalCounter">01/09<h2>
+        </div>
+    </div>
+</div>
+
+<script src="../lager/l_lager.js"></script>
--- lager/l_lager.js
+++ lager/l_lager.js
@@ -3,8 +3,30 @@ $(document).ready(function () {
   //$(window).resize(sizeContent);
 });
 
+var regalCounter = 1;
 
-function sizeContent() {
-  var newHeight = $("html").height() + "px";
-  $(".wrapper").css("min-height", newHeight); 
-}
+
+$('#regalChangeRight').click(function(){
+   if(regalCounter < 9){
+       regalCounter++;       
+   } 
+   else{
+       regalCounter = 1;
+   }
+   $('#reagalCounter').html("0"+regalCounter+"/09");
+});
+
+$('#regalChangeLeft').click(function(){
+   if(regalCounter > 1){
+       regalCounter--;       
+   } 
+   else{
+       regalCounter = 9;
+   }
+   $('#reagalCounter').html("0"+regalCounter+"/09");
+});
+
+$('.lager_fach_button').click(function(){
+   console.log((this).id); 
+   alert("new Item for "+regalCounter+(this).id);
+});
