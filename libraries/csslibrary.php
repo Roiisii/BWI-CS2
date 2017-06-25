a70aeabdf81f7873766cdad6fe06f93d89609827
joa <joa@DESKTOP-5KNRT1M.local>
2017-06-25 08:08:21 +0200

    html tags removed

--- libraries/csslibrary.php
+++ libraries/csslibrary.php
@@ -1,4 +1,2 @@
-<html>   
-    <link href="../libraries/bootstrap/css/bootstrap.min.css" rel="stylesheet"> <!--Bootstrap-->   
-    <link href="../css/custom.css" rel="stylesheet"> <!--MyStylesheet-->    
-</html>
+<link href="../libraries/bootstrap/css/bootstrap.min.css" rel="stylesheet"> <!--Bootstrap-->   
+<link href="../css/custom.css" rel="stylesheet"> <!--MyStylesheet-->    
--- libraries/jslibrary.php
+++ libraries/jslibrary.php
@@ -1,4 +1,2 @@
- <html>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script> <!--jQuery-->
-        <script src="../libraries/bootstrap/js/bootstrap.min.js"></script> <!--Bootstrap-->        
-</html>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script> <!--jQuery-->
+<script src="../libraries/bootstrap/js/bootstrap.min.js"></script> <!--Bootstrap-->        
