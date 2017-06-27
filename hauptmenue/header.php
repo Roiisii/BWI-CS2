20c388f6a98183a75bbbceca7b10b7979e3e6e5c
Christian <christian.rois.cr@gmail.com>
2017-06-27 21:51:15 +0200

    Logout eingebaut

--- hauptmenue/header.php
+++ hauptmenue/header.php
@@ -11,7 +11,13 @@
             <div class="row">
                 <div class="col-md-4"></div>
                 <div class="col-md-6"><h1>Boosted Logistics OS</h1></div>
-                <div class="col-md-2"><button type="button" class="btn btn-primary btn-sm">Logout</button></div>
+                <div class="col-md-2"><button type="button" id="btn_logout" class="btn btn-primary btn-sm">Logout</button></div>
+                <script>
+                    $('#btn_logout').click(function(){
+                       location.href = "../login/s_logOut.php";
+                    });
+                </script>
+                
             </div>   
        </div> 
 
